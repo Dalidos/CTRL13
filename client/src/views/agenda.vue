6ad33a4f7ef9d384a5c5852053b0187b8e016ce4
anthony.schwartz <anthony.schwartz@uha.fr>
2018-12-07 00:51:28 +0100

    *-test form add event

--- client/src/views/agenda.vue
+++ client/src/views/agenda.vue
@@ -1,8 +1,17 @@
 
 
 <template>
-    <div class="about border">
+    <div class="about">
         <h1>This is an agenda page</h1>
+        <!-- add event into calendar -->
+        <section>
+            <div class="form-group row">
+                <input type="text" class="form-control" placeholder="titre de l'évènement">
+                <input type="date" class="form-control" value="">
+                <button class="btn btn-primary">Ajouter l'évènement</button>
+            </div>
+        </section>
+        <!-- calendar -->
         <full-calendar :events="events"></full-calendar>
     </div>
 </template>
