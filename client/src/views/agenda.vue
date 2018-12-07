b6c7a67f55cc3913e8a231ea5d00a9fec1d9b415
anthony.schwartz <anthony.schwartz@uha.fr>
2018-12-07 04:07:56 +0100

    *-merge avec calendar

--- client/src/views/agenda.vue
+++ client/src/views/agenda.vue
@@ -2,21 +2,7 @@
 
 <template>
     <div class="about">
-        <h1>This is an agenda page</h1>
-        <!-- add event into calendar -->
-        <section class="mt-5 mb-5">
-            <div class="shadow rounded form-group p-2 w-100 d-flex justify-content-center">
-                <div class="col-3">
-                    <input type="text" class="form-control" placeholder="titre de l'évènement">
-                </div>
-                <div class="col-3">
-                    <input type="date" class="form-control" value="">
-                </div>
-                <div class="col-3">
-                    <button class="btn btn-primary form-control">Ajouter l'évènement</button>
-                </div>
-            </div>
-        </section>
+
         <!-- calendar -->
         <div id='calendar'>
             <full-calendar :events="events" ref="calendar" @day-click="dayClick" @event-selected="eventClick"></full-calendar>
