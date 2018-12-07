46f6cb933cfbc3e6301064c86c282ae3b2802ae2
Dje-Dje <andre.jerome@protonmail.com>
2018-12-07 01:25:05 +0100

    todolist test

---
+++ client/src/views/Todolist.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="Todolist">
+        <ol>
+            <li v-for="item in items">
+                {{ item }}
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Todolist',
+        data() {
+            return {
+                items: [
+                    'Apprendre JavaScript',
+                    'Apprendre Vue',
+                    'Créer quelque chose de génial'
+                ]
+            }
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
