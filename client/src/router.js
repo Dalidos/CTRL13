e10b3cbf9fe9b61b2689c17a5142169e5f8daf47
florent.haffner <florent.haffner@gmail.com>
2018-12-07 13:18:46 +0100

    Snapshot du travail sur front-inventaire à la fin de la nuit

--- client/src/router.js
+++ client/src/router.js
@@ -21,10 +21,15 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
-      {
-        path: '/todolist',
-        name: 'todolist',
-        component: () => import('./views/Todolist.vue')
-      }
+    {
+      path: '/todolist',
+      name: 'todolist',
+      component: () => import('./views/Todolist.vue')
+    },
+    {
+      path: '/inventory',
+      name: 'inventory',
+      component: () => import('./views/Inventory.vue')
+    }
   ]
 })
---
+++ client/src/views/Inventory.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="Inventory">
+        <div class="text content mt-5 mb-5">
+            <h1>Inventaire</h1>
+        </div>
+
+        {{ product }}
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Nom</th>
+                <th scope="col">Quantité</th>
+                <th scope="col">Gestion</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Mark</td>
+                <td>X</td>
+                    <input type="number">
+                    <button @click="TODO">+1</button>
+            </tr>
+            <tr>
+                <td>Jacob</td>
+                <td>Y</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <td>Larry</td>
+                <td>Z</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    /* AJAX REQUEST */
+    var axios = require('axios');
+
+    /* RETURN RESULT */
+    export default {
+        name: 'Inventory',
+            data() {
+                return {
+                    product: []
+                }
+            },
+        created() {
+            fetch('https://api.myjson.com/bins/74l63')
+            .then(response => response.json())
+            .then(json => {
+                this.product = json.product
+            })
+        }
+    }
+</script>
